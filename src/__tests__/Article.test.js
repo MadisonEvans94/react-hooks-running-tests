65f6bebc3a20b505e05bee9145b655f3cd93340c
Madison Evans <m.corbinevans@gmail.com>
2023-02-03 09:04:41 -0700

    finished lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -5,7 +5,7 @@ import React from "react";
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
-  render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+	render(<Article />);
+	screen.debug();
+	expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
